501baa23239ee7723320191bdbb78bf656a0173a
Aline Camargo <alineandradecintra@gmail.com>
2024-04-19 04:51:36 -0300

    19/04/2024

---
+++ each_array.rb
@@ -0,0 +1,9 @@
+names = ['Joãozinho', 'Manoel', 'Juca']
+
+name = 'Aline Camargo'
+
+names.each do |name|
+    puts name + 'é o meu nome'
+end
+
+# puts name
---
+++ each_hash.rb
@@ -0,0 +1,6 @@
+aulas = {'Aula 1 ' => 'Liberada', 'Aula 2 ' => 'Liberada', 'Aula 3 ' => 'Em breve'}
+
+aulas.each do |key, value|
+    puts "#{key}" "#{value}"
+end
+
