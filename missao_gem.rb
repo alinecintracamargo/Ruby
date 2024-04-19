dcbda50a7b5c78d6a63d4028252587a3d5e8b047
Aline Camargo <alineandradecintra@gmail.com>
2024-04-19 06:23:02 -0300

    .

--- missao_gem.rb
+++ missao_gem.rb
@@ -1,2 +1,15 @@
-require cpf_cnpj
+require 'cpf_cnpj'
+
+def validation(cpf)
+    if CPF.valid?(cpf, strict: true)
+        puts "CPF Válido"
+    else
+        puts "CPF Inválido"
+    end
+end
+
+print ("Digite seu CPF: ")
+cpf = gets.chomp
+
+validation(cpf)
 
